e5f8e69279a22dd049c02b123c8397ce96a6d6f7
bhydrangea <forstudynanthanan@gmail.com>
2024-10-19 21:24:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# YOUR CODE HERE
+def summation(lst1, lst2):
+    updated_list = [x + y for x, y in zip(lst1, lst2)]
+    return updated_list
+
+def find_min_max(updated_list):
+    return min(updated_list), max(updated_list)
+
+n = int(input())
+lst1 = [int(input()) for k in range(n)]
+lst2 = [int(input()) for k in range(n)]
+
+updated_list = summation(lst1, lst2)
+min_value, max_value = find_min_max(updated_list)
+
+print(updated_list)
+print((min_value, max_value))
